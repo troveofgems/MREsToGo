d307c2ed164c8a2269612f150372479ae27644f8
Dustin Kent Greco <dkgreco@thetroveofgems.tech>
2022-03-04 11:44:01 -0700

    Initial Project Commit

--- App.js
+++ App.js
@@ -1,20 +1,49 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { StatusBar as ExpoStatusBar } from "expo-status-bar";
+import {
+  StatusBar,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  View,
+  Platform,
+} from "react-native";
+import { Searchbar } from "react-native-paper";
+
+const isAndroidDevice = Platform.OS === "android";
 
 export default function App() {
+  const [searchTerm, setSearchTerm] = useState("");
+  const onChangeSearch = (query) => setSearchTerm(`${query}`);
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.search}>
+        <Searchbar
+          placeholder="Search"
+          onChangeText={onChangeSearch}
+          value={searchTerm}
+        />
+      </View>
+      <View style={styles.list}>
+        <Text>App Body</Text>
+      </View>
+      <ExpoStatusBar style="auto" />
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: isAndroidDevice ? StatusBar.currentHeight : 0,
+  },
+  search: {
+    backgroundColor: "green",
+    padding: 16,
+  },
+  list: {
+    backgroundColor: "blue",
+    padding: 16,
+    flex: 1,
   },
 });
