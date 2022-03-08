eabd2c33cc8432fb8d08303ed2ca02d8af69a20c
Dustin Kent Greco <dkgreco@thetroveofgems.tech>
2022-03-08 08:37:20 -0700

    Prepped For Services Mock

--- src/services/installations/installations.context.js
+++ src/services/installations/installations.context.js
@@ -2,6 +2,7 @@ import { mocks } from "./mock";
 
 export const installationsRequest = ({ lat, lng }) => {
   return new Promise((resolve, reject) => {
+    console.log(mocks);
     const mock = mocks[location];
     if (!mock) reject("Not Found");
     resolve(mock);
